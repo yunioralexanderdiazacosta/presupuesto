048bb84532a6bb4f66c929b1a6c3b1180b08869c
gonza1414 <gquezada.cyt@gmail.com>
2025-04-23 21:03:23 -0400

    creacion de insumos

---
+++ app/Http/Controllers/Supplies/DeleteSupplyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Supplies;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeleteSupplyController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Supplies/StoreSupplyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Supplies;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StoreSupplyController extends Controller
+{
+    //
+}
