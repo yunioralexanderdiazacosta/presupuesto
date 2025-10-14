19f2a970d005dec896606adcbdf49cb820814b44
gonza1414 <gquezada.cyt@gmail.com>
2025-10-14 17:26:51 -0300

    feat

--- resources/js/Components/Groupings/EditGroupingModal.vue
+++ resources/js/Components/Groupings/EditGroupingModal.vue
@@ -1,9 +1,13 @@
 <script setup>
-import GroupingForm from './GroupingForm.vue';
+import GroupingForm from './GroupingFormMultiple.vue';
 import Modal from '@/Components/Modal.vue';
 
 const props = defineProps({
-    form: Object
+    form: Object,
+    costCenters: {
+        type: Array,
+        default: () => [],
+    }
 });
 
 defineEmits(['update']);
@@ -13,17 +17,17 @@ defineEmits(['update']);
         <template #header>
             <div class="d-flex align-items-center gap-2 mb-3 text-start">
                 <span class="bg-secondary text-white rounded-circle d-flex align-items-center justify-content-center" style="width: 38px; height: 38px; font-size: 1.4rem;">
-                    <i class="fas fa-seedling"></i>
+                    <i class="fas fa-object-group"></i>
                 </span>
                 <span>
-                    <span class="fw-bold" style="font-size: 1.2rem; color: #2d3748; letter-spacing: 0.5px;">Editar Cosecha</span>
+                    <span class="fw-bold" style="font-size: 1.2rem; color: #2d3748; letter-spacing: 0.5px;">Editar Agrupación</span>
                     <br>
-                    <span class="text-muted ml-1" style="font-size: 0.85rem;">Modificar los datos</span>
+                    <span class="text-muted ml-1" style="font-size: 0.85rem;">Modificar los datos del grupo</span>
                 </span>
             </div>
         </template>
         <template #body>
-            <GroupingForm :form="form" />
+            <GroupingForm :form="form" :cost-centers="costCenters" />
         </template>
         <template #footer>
             <button type="button" id="kt_modal_update_grouping_cancel" data-bs-dismiss="modal" class="btn btn-light me-3">Cerrar</button>
