86681b4a20b51f7d1fb4bc9351419b056e45a840
gonza1414 <gquezada.cyt@gmail.com>
2026-02-03 21:54:29 -0300

    feat

--- resources/js/Pages/CreditDebitNotes.vue
+++ resources/js/Pages/CreditDebitNotes.vue
@@ -102,6 +102,10 @@ const openEdit = async (note) => {
     const fullNote = response.data.note;
     const items = response.data.items;
     
+    console.log('Full response from backend:', response.data);
+    console.log('fullNote object:', fullNote);
+    console.log('fullNote.supplier_id:', fullNote.supplier_id);
+    console.log('fullNote.invoice_id:', fullNote.invoice_id);
     console.log('Items from backend:', items);
     
     form.id = fullNote.id;
