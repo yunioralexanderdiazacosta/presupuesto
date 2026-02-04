82e0642fa18fc61249d585b82a38a387034374f8
gonza1414 <gquezada.cyt@gmail.com>
2026-02-03 21:49:23 -0300

    feat

--- resources/js/Pages/CreditDebitNotes/Edit.vue
+++ resources/js/Pages/CreditDebitNotes/Edit.vue
@@ -8,4 +8,9 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import FormCreditDebitNote from '@/Components/CreditDebitNotes/Form.vue';
 const props = defineProps({ note: Object, suppliers: Array, invoices: Array, products: Array, units: Array, items: Array });
+
+// Debug temporal
+console.log('Note object from backend:', props.note);
+console.log('Note supplier_id:', props.note?.supplier_id);
+console.log('Note invoice_id:', props.note?.invoice_id);
 </script>
